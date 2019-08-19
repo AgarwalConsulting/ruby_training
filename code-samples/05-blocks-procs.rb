1c90726d05ded31c1038c48e2e29453a4eb834dd
Gaurav Agarwal <algogrit@gmail.com>
2019-08-19 15:31:51 +0530

    Adding procs, blocks and yield.

--- code-samples/05-blocks-procs.rb
+++ code-samples/05-blocks-procs.rb
@@ -1,10 +1,54 @@
 require 'pp'
-
-array = [1, 2, 3]
+require 'pry'
 
 double_proc = Proc.new {|val| val * 2 }
 
-new_array = array.map(&double_proc)
-
+array = [1, 2, 3]
 pp array
+
+new_array = array.map {|val| val * 2}
+
 pp new_array
+
+class Student
+  attr_accessor :name, :score
+
+  def get_score
+    @score
+  end
+
+  def update(&block)
+    # binding.pry
+    @name = "Student from class A - #{@name}"
+    update_score &block
+  end
+
+  def update_score(&block)
+    binding.pry
+
+    if block
+      @score = block.call(@score)
+    else
+      @score = @score + 1
+    end
+  end
+
+  # def update_score
+  #   if block_given?
+  #     @score = yield @score
+  #   else
+  #     @score = @score + 1
+  #   end
+  # end
+end
+
+s = Student.new
+s.name = "G"
+s.score = 50
+
+# s.get_score &double_proc
+
+s.update &double_proc
+# s.update_score &double_proc
+
+pp s
