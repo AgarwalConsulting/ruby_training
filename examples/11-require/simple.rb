b92b99dc3ebbbb440909202aa66f955eb7d5a1ea
Gaurav Agarwal <algogrit@gmail.com>
2021-09-02 22:07:35 +0530

    Adding require & require_relative.

---
+++ examples/11-require/simple.rb
@@ -0,0 +1,10 @@
+# require "./greet/greeting"
+# require_relative "greet/greeting"
+
+# puts greeting "World"
+
+
+require "../10-higer-order-functions/adder_class"
+# require_relative "../10-higer-order-functions/adder_class"
+
+puts Adder.new 2
