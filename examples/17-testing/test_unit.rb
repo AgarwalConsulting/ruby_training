fda37563609d8cbc3567ff74116763e7bfd5b3df
Gaurav Agarwal <algogrit@gmail.com>
2021-09-08 21:09:57 +0530

    Adding test unit and rspec examples.

---
+++ examples/17-testing/test_unit.rb
@@ -0,0 +1,9 @@
+require "test/unit/assertions"
+
+include Test::Unit::Assertions
+
+hello = 'world'
+
+assert_equal 'world', hello, "hello function should return 'world'"
+
+flunk "throw a failure message"
