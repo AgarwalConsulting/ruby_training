08bc240a6f256678340436313655867d6504c8be
Gaurav Agarwal <algogrit@gmail.com>
2025-08-05 10:56:23 +0530

    Adding popen3 example.

--- examples/21-processes/open3.rb
+++ examples/21-processes/open3.rb
@@ -1,6 +1,12 @@
 require 'open3'
 
-Open3.popen2("cat") do |stdin, stdout, wait_thr|
+# Open3.popen2("cat") do |stdin, stdout, wait_thr|
+#   stdin.puts "Open3 input"
+#   stdin.close
+#   puts "Output: #{stdout.read}"
+# end
+
+Open3.popen3("cat") do |stdin, stdout, stderr, wait_thr|
   stdin.puts "Open3 input"
   stdin.close
   puts "Output: #{stdout.read}"
